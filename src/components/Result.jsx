9525fb280914c295c0cd5942d2c30e7fcccac90e
Baris G <barisg@pm.me>
2023-11-02 15:42:50 +0100

    avancement

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,43 +2,47 @@ import data from "../data/data.json";
 
 function Result() {
   return (
-    <div className="result w-1/2 bg-white p-10 flex flex-col gap-8">
+    <div className="result w-1/2 max-md:w-full bg-white p-10 flex flex-col gap-8">
       <p className="text-2xl font-bold">Summary</p>
       <div className="detail flex flex-col gap-5">
         <div className="reaction w-72 h-14 bg-black flex items-center p-4 justify-between">
           <div className="flex gap-2">
             <img src={data[0].icon} alt="icon" />
-            <p>{data[0].category}</p>
+            <p className="font-bold">{data[0].category}</p>
           </div>
-          <div className="flex">
-            <span className="font-extrabold">{data[0].score}</span> / 100
+          <div className="flex gap-2 text-black">
+            <p className="font-extrabold">{data[0].score}</p>
+            <p className="text-slate-400"> / 100</p>
           </div>
         </div>
         <div className="memory w-72 h-14 bg-black flex items-center p-4 justify-between">
           <div className="flex gap-2">
             <img src={data[1].icon} alt="icon" />
-            <p>{data[1].category}</p>
+            <p className="font-bold">{data[1].category}</p>
           </div>
-          <div className="flex">
-            <span className="font-extrabold">{data[1].score}</span> / 100
+          <div className="flex gap-2 text-black">
+            <p className="font-extrabold">{data[1].score}</p>
+            <p className="text-slate-400"> / 100</p>
           </div>
         </div>
         <div className="verbal w-72 h-14 bg-black flex items-center p-4 justify-between">
           <div className="flex gap-2">
             <img src={data[2].icon} alt="icon" />
-            <p>{data[2].category}</p>
+            <p className="font-bold">{data[2].category}</p>
           </div>
-          <div className="flex">
-            <span className="font-extrabold">{data[2].score}</span> / 100
+          <div className="flex gap-2 text-black">
+            <p className="font-extrabold">{data[2].score}</p>
+            <p className="text-slate-400"> / 100</p>
           </div>
         </div>
         <div className="visual w-72 h-14 bg-black flex items-center p-4 justify-between">
           <div className="flex gap-2">
             <img src={data[3].icon} alt="icon" />
-            <p>{data[3].category}</p>
+            <p className="font-bold">{data[3].category}</p>
           </div>
-          <div className="flex">
-            <span className="font-extrabold">{data[3].score}</span> / 100
+          <div className="flex gap-2 text-black">
+            <p className="font-extrabold">{data[3].score}</p>
+            <p className="text-slate-400"> / 100</p>
           </div>
         </div>
       </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,10 +3,10 @@ import Result from "../components/Result";
 function App() {
   return (
     <>
-      <div className="container shadow-2xl">
-        <div className="info w-1/2 grid p-10 px-14 text-center">
+      <div className="container flex">
+        <div className="info w-1/2 flex flex-col gap-6 p-10 px-14 items-center text-center">
           <p className="text-2xl">Your Result</p>
-          <div className="score rounded-full h-48 w-48 bg-white justify-self-center flex flex-col justify-center items-center">
+          <div className="score rounded-full h-52 w-52 bg-white justify-self-center flex flex-col justify-center items-center">
             <p className="text-7xl font-extrabold text-white">76</p>
             <p>of 100</p>
           </div>
